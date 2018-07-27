c02d42d98382424e322671854f6656c4b9cea6f1
AnilAleti <anil.javaws@gmail.com>
2018-07-27 19:07:12 +0530

    Added rest client

--- src/main/java/com/sreenu/core/RestResource.java
+++ src/main/java/com/sreenu/core/RestResource.java
@@ -1,19 +1,27 @@
 package com.sreenu.core;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 @Path("/hello")
 public class RestResource {
 	
 	@GET
-	@Path("/{param}")
-	public Response getMsg(@PathParam("param") String msg) {
+	//@Path("/{param}")
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response getMsg(@QueryParam("param") String msg, @HeaderParam("Content-Type") String content) {
  
-		String output = "Jersey say : " + msg;
+		String output = "Jersey say : " + msg+ "header"+content;
  
 		return Response.status(200).entity(output).build();
+		
  
 	}
 
