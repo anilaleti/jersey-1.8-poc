8e7f0524883885823b22138eea837454bdc1d376
AnilAleti <anil.javaws@gmail.com>
2018-07-30 15:56:54 +0530

    formatted code

--- src/main/java/com/sreenu/core/RestResource.java
+++ src/main/java/com/sreenu/core/RestResource.java
@@ -4,25 +4,34 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+/**
+ * @author Anil Aleti
+ *
+ */
 @Path("/hello")
+
 public class RestResource {
-	
+
+	/**
+	 * 
+	 * @param msg
+	 * @param content
+	 * @return Response
+	 */
 	@GET
-	//@Path("/{param}")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.TEXT_PLAIN)
 	public Response getMsg(@QueryParam("param") String msg, @HeaderParam("Content-Type") String content) {
- 
-		String output = "Jersey say : " + msg+ "header"+content;
- 
+
+		String output = "Jersey say : " + msg + "header" + content;
+
 		return Response.status(200).entity(output).build();
-		
- 
+
 	}
 
 }
