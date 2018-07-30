6c639d5beeb58652e046100106a3f613ba8c55d7
AnilAleti <anil.javaws@gmail.com>
2018-07-30 15:57:56 +0530

    formatted code

--- src/main/java/com/sreenu/core/client/RestClient.java
+++ src/main/java/com/sreenu/core/client/RestClient.java
@@ -1,33 +1,33 @@
 package com.sreenu.core.client;
 
-import javax.ws.rs.core.MultivaluedMap;
-
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
+
+/**
+ * 
+ * @author Anil Aleti
+ *
+ */
 
 public class RestClient {
-	
+
 	
 	public static void main(String[] args) {
-		
-		
-		Client client = Client.create();
-		WebResource webResource =client.resource("http://localhost:9090/jersey-1.8-poc/rest/hello/");
 
-		/*MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
-		queryParams.add("param", "hi"); //set parametes for request
-*/
-				ClientResponse response = null;
-		response = webResource.queryParam("param","hi").header("Content-Type", "english")
-		                        .get(ClientResponse.class);
-		
+		Client client = Client.create();
+		WebResource webResource = client.resource("http://localhost:9090/jersey-1.8-poc/rest/hello/");
+
+		/*
+		 * MultivaluedMap<String, String> queryParams = new
+		 * MultivaluedMapImpl(); queryParams.add("param", "hi"); //set parametes
+		 * for request
+		 */
+		ClientResponse response = null;
+		response = webResource.queryParam("param", "hi").header("Content-Type", "english").get(ClientResponse.class);
+
 		System.out.println(response.getEntity(String.class));
-		
-		
-		
-		
+
 	}
 
 }
